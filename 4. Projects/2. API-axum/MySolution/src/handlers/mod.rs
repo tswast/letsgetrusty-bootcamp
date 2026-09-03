3240e38168c9421c6d6a57c792f62827ad72825a
Tim Swena <swast@google.com>
2026-09-03 09:29:57 -0500

    stage 1 steps 1 & 2

--- 4. Projects/2. API-axum/MySolution/src/handlers/mod.rs
+++ 4. Projects/2. API-axum/MySolution/src/handlers/mod.rs
@@ -13,11 +13,18 @@ pub async fn create_question(Json(question): Json<Question>) -> impl IntoRespons
 }
 
 pub async fn read_questions() -> impl IntoResponse {
-    let () = todo!();
+    Json(vec![
+        QuestionDetail {
+            question_uuid: "d347261c-3f0e-42d2-8706-5ef9f1b96725".to_owned(),
+            title: "Newly Created Question".to_owned(),
+            description: "My Description".to_owned(),
+            created_at: "2022-12-31 18:44:08.287442".to_owned(),
+        }
+    ])
 }
 
 pub async fn delete_question(Json(question_uuid): Json<QuestionId>) {
-    let () = todo!();
+    ()
 }
 
 // ---- CRUD for Answers ----
@@ -27,7 +34,12 @@ pub async fn delete_question(Json(question_uuid): Json<QuestionId>) {
 //
 //       hint: this function should look very similar to the create_question function above
 pub async fn create_answer(Json(answer): Json<Answer>) -> impl IntoResponse {
-    let () = todo!();
+    Json(AnswerDetail {
+        answer_uuid: "a1a14a9c-ab9e-481b-8120-67f675531ed2".to_owned(),
+        question_uuid: "b068cd2f-edac-479e-98f1-c5f91008dcbd".to_owned(),
+        content: "test question".to_owned(),
+        created_at: "2022-12-31 13:11:59.728682".to_owned(),
+    })
 }
 
 // TODO: Create a GET route to /answers which accepts an `QuestionId` and returns a vector of `AnswerDetail` as JSON.
@@ -35,7 +47,14 @@ pub async fn create_answer(Json(answer): Json<Answer>) -> impl IntoResponse {
 //
 //       hint: this function should look very similar to the read_questions function above
 pub async fn read_answers() -> impl IntoResponse {
-    let () = todo!();
+    Json(vec![
+        AnswerDetail {
+            answer_uuid: "a1a14a9c-ab9e-481b-8120-67f675531ed2".to_owned(),
+            question_uuid: "b068cd2f-edac-479e-98f1-c5f91008dcbd".to_owned(),
+            content: "test question".to_owned(),
+            created_at: "2022-12-31 13:11:59.728682".to_owned(),
+        }
+    ])
 }
 
 // TODO: Create a DELETE route to /answer which accepts an `AnswerId` and does not return anything.
@@ -43,5 +62,5 @@ pub async fn read_answers() -> impl IntoResponse {
 //
 //       hint: this function should look very similar to the delete_question function above
 pub async fn delete_answer(Json(answer_uuid): Json<AnswerId>) {
-    let () = todo!();
+    ()
 }
